7223fa0e65964a87ed2caa9e234d7612c9cf2649
Basyrov Rustam <hrustbas@gmail.com>
2025-05-01 13:40:00 +0300

    fix headers prefix

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ set -xe
 gcc 				\
     src/file2mat.c              \
     --shared			\
-    -I /usr/include/python3.11	\
+    -I ${CONDA_PREFIX}/lib/python3.13/site-packages/numpy/_core/include \
+    -I ${CONDA_PREFIX}/include/python3.13 \
     -fPIC                       \
     -o file2mat.so
