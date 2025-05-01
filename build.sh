a65a70656059c17ff8b5c31e293ebcea81e69021
Basyrov Rustam <hrustbas@gmail.com>
2025-05-01 17:18:07 +0300

    fix build script

--- build.sh
+++ build.sh
@@ -2,15 +2,10 @@
 
 set -xe
 
-# gcc \
-#     src/file2data.c \
-#     --shared \
-#     -o file2data.so
-
-gcc 				\
-    src/file2mat.c              \
-    --shared			\
+gcc									\
+    src/file2mat.c							\
+    --shared								\
     -I ${CONDA_PREFIX}/lib/python3.13/site-packages/numpy/_core/include \
-    -I ${CONDA_PREFIX}/include/python3.13 \
-    -fPIC                       \
+    -I ${CONDA_PREFIX}/include/python3.13				\
+    -fPIC								\
     -o file2mat.so
